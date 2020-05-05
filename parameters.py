a84603d99ffbc960547d7e75aa769d91e23974fc
jessicamegane <jessicacunha98@hotmail.com>
2020-05-06 00:24:52 +0100

    new parameters

--- parameters.py
+++ parameters.py
@@ -8,9 +8,13 @@ params = {
     "TOURNAMENT": 3,
     "ELITISM": 10,
     "NR_CUTS": 1,
-    "SIZE_GENOTYPE": knapsack["NR_ITEMS"],    # NR ITEMS
-    "METHOD": 2,
+    "SIZE_GENOTYPE": kp["NR_ITEMS"],    # NR ITEMS
+    "METHOD": 1,
     "INDIVIDUALS_REPLACE": 3,
+    "INDIVIDUALS_EXCHANGE": 5,
+    "EXCHANGE_METHOD": "random",      # worst, best, random
+    "MIGRATION": 1,      # 1 - always, 2 - each 2 generations, 3 - each X gen, a partir de X
+    "MIGRATION_N": 5,
 }
 
 
