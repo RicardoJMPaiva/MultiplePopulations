5adb5b11237edcca360b78014962724723d3dbfc
jessicamegane <jessicacunha98@hotmail.com>
2020-05-06 00:23:25 +0100

    changed var name

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import random
-# Knapsack
+# knapsack
 
-knapsack = {
+kp = {
     "V": 10,
     "R": 5,
     "NR_ITEMS": 100,    # 100, 250, 500
@@ -9,25 +9,25 @@ knapsack = {
 }
 
 def generate_uncor():
-    weights = [random.uniform(1,knapsack["V"]) for i in range(knapsack["NR_ITEMS"])]
-    values = [random.uniform(1,knapsack["V"]) for i in range(knapsack["NR_ITEMS"])]
+    weights = [random.uniform(1,kp["V"]) for i in range(kp["NR_ITEMS"])]
+    values = [random.uniform(1,kp["V"]) for i in range(kp["NR_ITEMS"])]
     capacity = int(0.5 * sum(weights))
     return {'weights':weights, 'values':values, 'capacity': capacity}
 
 def generate_weak_cor():
-    weights = [random.uniform(1,knapsack["V"]) for i in range(knapsack["NR_ITEMS"])]
+    weights = [random.uniform(1,kp["V"]) for i in range(kp["NR_ITEMS"])]
     values = []
-    for i in range(knapsack["NR_ITEMS"]):
-        value = weights[i] + random.uniform(-knapsack["R"],knapsack["R"])
+    for i in range(kp["NR_ITEMS"]):
+        value = weights[i] + random.uniform(-kp["R"],kp["R"])
         while value <= 0:
-            value = weights[i] + random.uniform(-knapsack["R"],knapsack["R"])
+            value = weights[i] + random.uniform(-kp["R"],kp["R"])
         values.append(value)
     capacity = int(0.5 * sum(weights))
     return {'weights':weights, 'values':values, 'capacity': capacity}
 
 def generate_strong_cor():
-    weights = [random.uniform(1,knapsack["V"]) for i in range(knapsack["NR_ITEMS"])]
-    values = [weights[i] + knapsack["R"] for i in range(knapsack["NR_ITEMS"])]
+    weights = [random.uniform(1,kp["V"]) for i in range(kp["NR_ITEMS"])]
+    values = [weights[i] + kp["R"] for i in range(kp["NR_ITEMS"])]
     capacity = int(0.5 * sum(weights))
     return {'weights':weights, 'values':values, 'capacity': capacity}
 
